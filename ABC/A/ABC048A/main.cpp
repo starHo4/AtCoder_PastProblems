c39f7473b82cd4f71b9ccc3b1c523d2dedcb7069
starHo4 <hi6yukiho4@gmail.com>
2021-07-09 18:25:41 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/A/ABC048A/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string A, s, C;
+    cin >> A >> s >> C;
+    string out_A, out_s, out_C;
+    out_A = A[0];
+    out_s = s[0];
+    out_C = C[0];
+    cout << out_A << out_s << out_C << endl;
+}
