e05778cbfa7b6d6611e7beecec67bf635c24e416
starHo4 <hi6yukiho4@gmail.com>
2021-07-16 20:01:24 +0900

    Confirm the correct behavior (AC)

--- ABC/A/ABC175A/main.cpp
+++ ABC/A/ABC175A/main.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    string S;
+    cin >> S;
+    int c = 0;
+    bool f_c = false;
+    for (int i = 0; i < 3; i++)
+    {
+        if (S[i] == 'R' && c == 0)
+        {
+            c++;
+            f_c = true;
+        }
+        else if (S[i] == 'R' && f_c)
+        {
+            c++;
+        }
+        else
+        {
+            f_c = false;
+        }
+    }
+    cout << c << endl;
+}
