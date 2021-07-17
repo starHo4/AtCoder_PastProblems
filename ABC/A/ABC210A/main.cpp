322b9e1164a593b3796287514ce44102f4007139
starHo4 <hi6yukiho4@gmail.com>
2021-07-17 22:44:28 +0900

    Confirm the correct behavior (AC)

--- ABC/A/ABC210A/main.cpp
+++ ABC/A/ABC210A/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int N, A, X, Y;
+    cin >> N >> A >> X >> Y;
+    int ans = N * X;
+    if (N > A)
+    {
+        ans = A * X + (N - A) * Y;
+    }
+    cout << ans << endl;
 }
--- ABC/B/ABC210B/main.cpp
+++ ABC/B/ABC210B/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int N;
+    string S;
+    cin >> N >> S;
+    string out = "Takahashi";
+    for(int i=0; i<N; i++){
+        if(S[i] == '1'){
+            if(i%2){
+                out = "Aoki";
+            }
+            break;
+        }
+    }
+    cout << out << endl;
 }
