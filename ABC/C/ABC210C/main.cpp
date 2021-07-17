f3e1869bffd634835a035da9969b65e0d3186124
starHo4 <hi6yukiho4@gmail.com>
2021-07-17 22:44:39 +0900

    Not yet ...

--- ABC/C/ABC210C/main.cpp
+++ ABC/C/ABC210C/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    vector<int> c(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> c[i];
+    }
+    int ans = 0;
+    for (int i = 0; i < N - K + 1; i++)
+    {
+        vector<int> tmp(K);
+        for (int j = i; j < i + K; j++)
+        {
+            tmp[j-i] = c[j];
+        }
+        sort(tmp.begin(), tmp.end());
+        decltype(tmp)::iterator result = unique(tmp.begin(), tmp.end());
+        tmp.erase(result, tmp.end());
+        int c_num = tmp.size();
+        if(ans < c_num){
+            ans = c_num;
+        }
+    }
+    cout << ans << endl;
 }
--- ABC/D/ABC210D/main.cpp
+++ ABC/D/ABC210D/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+using vi = vector<int>;
+using vvi = vector<vi>;
+typedef long long ll;
 int main(){
-    
+    int H, W, C;
+    cin >> H >> W >> C;
+    vvi A(H, vi(W));
+    for(int i=0; i<H; i++){
+        for(int j=0; j<W; j++){
+            cin >> A[i][j];
+        }
+    }
 }
