778a63ecc716d679b1df6a9dca2977fa916169d6
starHo4 <hi6yukiho4@gmail.com>
2021-07-19 22:57:25 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/A/ABC139A/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string S, T;
+    cin >> S >> T;
+    int ans = 0;
+    for(int i=0; i<S.size(); i++){
+        if(S[i]==T[i]){
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
