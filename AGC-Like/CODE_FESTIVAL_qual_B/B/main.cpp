bcaee1e7eb895ce9aa0fc0ac003ab7092736644d
starHo4 <hi6yukiho4@gmail.com>
2021-07-20 15:26:07 +0900

    Confirm the correct behavior (AC)

--- AGC-Like/CODE_FESTIVAL_qual_B/B/main.cpp
+++ AGC-Like/CODE_FESTIVAL_qual_B/B/main.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+int main()
+{
+    int N, A, B;
+    string S;
+    cin >> N >> A >> B >> S;
+    vector<string> ans(N, "No");
+    int count = 0;
+    int b_count = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (S[i] == 'c')
+        {
+            cout << ans[i] << endl;
+            continue;
+        }
+        if (S[i] == 'a')
+        {
+            if (count < A + B)
+            {
+                count++;
+                ans[i] = "Yes";
+            }
+        }
+        if (S[i] == 'b')
+        {
+            if (count < A + B && b_count < B)
+            {
+                count++;
+                b_count++;
+                ans[i] = "Yes";
+            }
+        }
+        cout << ans[i] << endl;
+    }
+}
