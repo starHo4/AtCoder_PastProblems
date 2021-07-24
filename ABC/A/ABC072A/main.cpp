4e80c738f5424df6a63ec5409356080dc14de610
starHo4 <hi6yukiho4@gmail.com>
2021-07-24 10:41:14 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/A/ABC072A/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int X, t;
+    cin >> X >> t;
+    if(X > t){
+        cout << X - t << endl;
+    } else {
+        cout << 0 << endl;
+    }
+}
