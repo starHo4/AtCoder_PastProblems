8bc6d6c8a8e1c601d67dcdaa08ba351f80c4f704
starHo4 <hi6yukiho4@gmail.com>
2021-07-27 14:08:23 +0900

    Confirm the correct  behavior (AC)

---
+++ ABC/B/ABC143B/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    int d[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> d[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            sum += d[i] * d[j];
+        }
+    }
+    cout << sum << endl;
+}
