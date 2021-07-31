ef5abd123c5125f06d418429e8de8c2ddafb8be5
starHo4 <hi6yukiho4@gmail.com>
2021-07-31 14:01:04 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/B/ABC132B/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> p[i];
+    }
+    int ans = 0;
+    for (int i = 1; i < n - 1; i++)
+    {
+        vector<int> tmp;
+        tmp.push_back(p[i - 1]);
+        tmp.push_back(p[i]);
+        tmp.push_back(p[i + 1]);
+        sort(tmp.begin(), tmp.end());
+        if (tmp[1] == p[i])
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
