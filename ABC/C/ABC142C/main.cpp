0581e3cc82e2ac2ab4da758f18be972876197e1b
starHo4 <hi6yukiho4@gmail.com>
2021-07-31 15:06:25 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/C/ABC142C/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    vector<int> ans(N);
+    for (int i = 0; i < N; i++)
+    {
+        ans[A[i] - 1] = i + 1;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        cout << ans[i];
+        if (i != N - 1)
+        {
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
