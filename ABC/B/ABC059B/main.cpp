9aa825583d7994abf51ea89b09542a5bc62e9deb
starHo4 <hi6yukiho4@gmail.com>
2021-08-07 14:20:55 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/B/ABC059B/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string A, B;
+    cin >> A >> B;
+    if (A.size() > B.size())
+    {
+        cout << "GREATER" << endl;
+    }
+    else if (A.size() < B.size())
+    {
+        cout << "LESS" << endl;
+    }
+    else
+    {
+        if (A > B)
+        {
+            cout << "GREATER" << endl;
+        }
+        else if (A < B)
+        {
+            cout << "LESS" << endl;
+        }
+        else
+        {
+            cout << "EQUAL" << endl;
+        }
+    }
+}
