d880242a1e43c601211d6c206669392441e2912b
starHo4 <hi6yukiho4@gmail.com>
2021-08-09 18:21:40 +0900

    Not yet ...

---
+++ ABC/C/ABC099C/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+const int INF = INT32_MAX / 2;
+int main(){
+    int N;
+    cin >> N;
+    vector<int> dp(N + 1, INF);
+    dp[0] = 0;
+    for(int i=1; i<N; i++){
+        
+    }
+}
