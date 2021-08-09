4cdf5ee1ce7ee872a83c7148295ff2596be4590a
starHo4 <hi6yukiho4@gmail.com>
2021-08-09 15:26:00 +0900

    Not yet ...

---
+++ AGC/A/AGC037A/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int solve(string crr, string rem){
+    if(crr != rem){
+        
+    }
+}
+int main(){
+    string S;
+    cin >> S;
+    int N = S.size();
+    int ans = solve("", S);
+    cout << ans << endl;
+}
