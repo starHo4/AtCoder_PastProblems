351196647bd7e5d706e5c6209a3e85e1e0472040
starHo4 <hi6yukiho4@gmail.com>
2021-08-09 20:18:12 +0900

    Not yet ...

---
+++ Others/Educational_DP_Contest/C/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    int hppns[N][3];
+    for(int i=0; i<N; i++){
+        cin >> hppns[i][0] >> hppns[i][1] >> hppns[i][2];
+    }
+    
+}
