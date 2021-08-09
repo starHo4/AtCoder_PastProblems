fd6c501a83375d3b14618c77e63a11c2e05f6f7c
starHo4 <hi6yukiho4@gmail.com>
2021-08-09 12:11:09 +0900

    Confirm the correct behavior (AC)

--- ABC/B/ABC093B/main.cpp
+++ ABC/B/ABC093B/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-int main(){
+int main()
+{
     int A, B, K;
     cin >> A >> B >> K;
+    vector<int> ans;
+    if (B - A + 1 <= 2 * K - 1)
+    {
+        for (int i = 0; i < B - A + 1; i++)
+        {
+            ans.push_back(A + i);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < K; i++)
+        {
+            ans.push_back(A + i);
+        }
+        for (int i = 0; i < K; i++)
+        {
+            ans.push_back(B - i);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
 }
