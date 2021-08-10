bc812ea4303741e0480da15fbaffc72f2adeae53
starHo4 <hi6yukiho4@gmail.com>
2021-08-10 22:27:48 +0900

    Confirm the correct behavior (AC)

--- Others/三井住友信託銀行プログラミングコンテスト2019/C/main.cpp
+++ Others/三井住友信託銀行プログラミングコンテスト2019/C/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
+int main()
+{
     int X;
     cin >> X;
+    vector<bool> dp(X + 110, false);
+    dp[0] = true;
+    for (int i = 0; i < X; i++)
+    {
+        if (dp[i])
+        {
+            for (int j = 0; j <= 5; j++)
+            {
+                dp[i + j + 100] = true;
+            }
+        }
+    }
+    if (dp[X])
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
 }
