a8c9b87e0d2cce45b4d55bba8bc33e95fd7546bc
starHo4 <hi6yukiho4@gmail.com>
2021-08-10 18:19:14 +0900

    Confirm the correct behavior (AC)

--- Others/Educational_DP_Contest/C/main.cpp
+++ Others/Educational_DP_Contest/C/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-int main(){
+int main()
+{
     int N;
     cin >> N;
-    int hppns[N][3];
-    for(int i=0; i<N; i++){
-        cin >> hppns[i][0] >> hppns[i][1] >> hppns[i][2];
+    vector<vector<int> > a(N, vector<int>(3));
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i][0] >> a[i][1] >> a[i][2];
     }
-    
+    vector<vector<int> > dp(N + 1, vector<int>(3, 0));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if (j == k)
+                {
+                    continue;
+                }
+                dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + a[i][k]);
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        ans = max(ans, dp[N][i]);
+    }
+    cout << ans << endl;
 }
