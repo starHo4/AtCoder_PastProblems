4d8ab5fe62073adfdc2a0d9a591c6884543e175b
starHo4 <hi6yukiho4@gmail.com>
2021-08-13 08:32:10 +0900

    Confirm the correct behavior (AC)

---
+++ ABC/A/ABC150A/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int K, X;
+    cin >> K >> X;
+    if (500 * K >= X)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
